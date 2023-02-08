1a76a948a0305a27e09810998a4d0fe787f60671
mknyambura <kariuki.mercie@gmail.com>
2023-02-08 22:38:54 +0300

    completed assignment

--- spec/environment_spec.rb
+++ spec/environment_spec.rb
@@ -1,3 +1,6 @@
+require_relative '../lib/garden.rb'
+require_relative '../lib/plant.rb'
+
 describe "environment.rb" do
   it "loads the code for the Garden and Plant classes" do
     expect { Garden }.not_to raise_error
